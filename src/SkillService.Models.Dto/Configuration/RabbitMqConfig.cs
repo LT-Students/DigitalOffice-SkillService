0e12146817d3c2e47fe8075b40bb54fae4881554
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2022-04-25 14:03:16 +0300

    add endpoint

--- src/SkillService.Models.Dto/Configuration/RabbitMqConfig.cs
+++ src/SkillService.Models.Dto/Configuration/RabbitMqConfig.cs
@@ -1,5 +1,6 @@
 ﻿using LT.DigitalOffice.Kernel.BrokerSupport.Attributes;
 using LT.DigitalOffice.Kernel.BrokerSupport.Configurations;
+using LT.DigitalOffice.Models.Broker.Publishing;
 using LT.DigitalOffice.Models.Broker.Requests.Skill;
 
 namespace LT.DigitalOffice.SkillService.Models.Dto.Configuration
@@ -8,5 +9,8 @@ namespace LT.DigitalOffice.SkillService.Models.Dto.Configuration
   {
     [AutoInjectRequest(typeof(IGetUserSkillsRequest))]
     public string GetUserSkillsEndpoint { get; set; }
+
+    [AutoInjectRequest(typeof(IDisactivateUserPublish))]
+    public string DisactivateUserEndpoint { get; set; }
   }
 }
