40fb1d3b3f2f1ba6194a11165f79bce3a8c507b2
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2022-04-27 15:31:28 +0300

    remove auto inject

--- src/SkillService.Models.Dto/Configuration/RabbitMqConfig.cs
+++ src/SkillService.Models.Dto/Configuration/RabbitMqConfig.cs
@@ -1,13 +1,9 @@
-﻿using LT.DigitalOffice.Kernel.BrokerSupport.Attributes;
-using LT.DigitalOffice.Kernel.BrokerSupport.Configurations;
-using LT.DigitalOffice.Models.Broker.Publishing;
-using LT.DigitalOffice.Models.Broker.Requests.Skill;
+﻿using LT.DigitalOffice.Kernel.BrokerSupport.Configurations;
 
 namespace LT.DigitalOffice.SkillService.Models.Dto.Configuration
 {
   public class RabbitMqConfig : BaseRabbitMqConfig
   {
-    [AutoInjectRequest(typeof(IGetUserSkillsRequest))]
     public string GetUserSkillsEndpoint { get; set; }
 
     public string DisactivateUserEndpoint { get; set; }
