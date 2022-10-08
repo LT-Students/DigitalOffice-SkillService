3b89d344df12a8eea4bfed714135720d9a2ba3f0
vasis <vasily.condratiev@yandex.ru>
2022-10-08 21:58:57 +0300

    Refactored FindUserSkillComand

--- src/SkillService.Business/Commands/UserSkill/FindUserSkillCommand.cs
+++ src/SkillService.Business/Commands/UserSkill/FindUserSkillCommand.cs
@@ -6,7 +6,7 @@ using LT.DigitalOffice.Kernel.Responses;
 using LT.DigitalOffice.SkillService.Business.Commands.UserSkill.Interfaces;
 using LT.DigitalOffice.SkillService.Data.Interfaces;
 using LT.DigitalOffice.SkillService.Mappers.Models.Interfaces;
-using LT.DigitalOffice.SkillService.Models.Db;
+//using LT.DigitalOffice.SkillService.Models.Db;
 using LT.DigitalOffice.SkillService.Models.Dto.Models;
 using MassTransit.Initializers;
 
@@ -27,10 +27,7 @@ public class FindUserSkillCommand : IFindUserSkillCommand
 
   public async Task<OperationResultResponse<List<ShortSkillInfo>>> ExecuteAsync(Guid userId)
   {
-    OperationResultResponse<List<ShortSkillInfo>> response = new();
-    List<DbSkill> dbSkillsList = await _userSkillRepository.FindAsync(userId);
-    response.Body = dbSkillsList.Select(_shotrSkillInfoMapper.Map).ToList();
-
-    return response;
+    return new OperationResultResponse<List<ShortSkillInfo>>(
+      body: (await _userSkillRepository.FindAsync(userId)).Select(_shotrSkillInfoMapper.Map).ToList());
   }
 }
